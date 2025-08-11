92c38ba2b585f586d0295b5c60d71f323364f756
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-08-11 15:39:09 +0900

    Initial Commit

---
+++ ThreadCloneApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadCloneApp
+//
+//  Created by 志水拓哉 on 2025/08/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadCloneApp/ThreadCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadCloneAppApp.swift
+//  ThreadCloneApp
+//
+//  Created by 志水拓哉 on 2025/08/11.
+//
+
+import SwiftUI
+
+@main
+struct ThreadCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
